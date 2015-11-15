243fe53c7c843227ef288a18d2031b8fc87edeb8
Jacob <jakeincanada@icloud.com>
2015-11-15 12:19:54 -0500

    Update main.js
    new debug, don't check own messages

--- main.js
+++ main.js
@@ -20,24 +20,28 @@ bot.on("disconnected", () => {
 });
 
 bot.on("message", (msg) => {
-	console.log(msg.content);
-	if( msg.content[0] === "!ip" || msg.content === "!address"){
-		//display server ip.
-		bot.reply( msg, "http://pvpcraft.ca" );
+	console.log('sender ' + msg.user.username + ' all ' + msg.content ' P0 ' + msg.content[0] + ' P1 ' + msg.content[1]);
+	if(msg.author.username === bot.user.username) {
 	}
+	else {
+		console.log(msg.content);
+		if( msg.content[0] === "!ip" || msg.content === "!address"){
+			//display server ip.
+			bot.reply( msg, "http://pvpcraft.ca" );
+		}
 	
-	if(msg.content[0] === '!invite' &&
-		msg.content[1].indexOf('discord.gg') > -1 &&
-		msg.channel instanceof Discord.PMChannel) {
-		client.joinServer(msg.content[1], function(err, server) {
-			if(err) {
-				bot.reply(msg, 'Something went wrong, please contact admins');
-			} else {
-				bot.reply(msg, 'Successfully joined ' + msg.server.name);
-			}
-		});
+		if(msg.content[0] === '!invite' &&
+			msg.content[1].indexOf('discord.gg') > -1 &&
+			msg.channel instanceof Discord.PMChannel) {
+			client.joinServer(msg.content[1], function(err, server) {
+				if(err) {
+					bot.reply(msg, 'Something went wrong, please contact admins');
+				} else {
+					bot.reply(msg, 'Successfully joined ' + msg.server.name);
+				}
+			});
+		}
 	}
-	
 });
 
 bot.login(AuthDetails.email, AuthDetails.password);
