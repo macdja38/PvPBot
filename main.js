ce34582e1419995df737fbbb54512dce266c333c
Jacob <jakeincanada@icloud.com>
2015-11-16 18:49:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -39,10 +39,10 @@ bot.on("userUpdate", (newUser, oldUser) => {
 	console.log("oldUser" + oldUser.username + " ID " + oldUser.id);
 	console.log("newUser" + newUser.username + " ID " + newUser.id);
 	for (server in bot.servers) {
-		console.log("Servers:" + servers);
-		for (member in server.members) {
-			console.log("Member:" + member.id);
-			if(oldUser.equals(member)){
+		console.log("Servers:" + bot.servers[server]);
+		for (member in servers[server].members) {
+			console.log("Member:" + servers[server].members[member].id);
+			if(oldUser.equals(servers[server].members[member].id)){
 				bot.sendMessage(server.defaultChannel, oldUser.username + 
 					" Just changed their name to " + newUser.username);
 			}
