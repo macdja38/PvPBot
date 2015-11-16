fec3d6d273bdf603898d5a63ae7c67f2fa4df7ed
Jacob <jakeincanada@icloud.com>
2015-11-16 14:30:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ bot.on("userUpdate", (newUser, oldUser) => {
 	console.log("oldUser" + oldUser.username + " ID " + oldUser.id);
 	console.log("newUser" + newUser.username + " ID " + newUser.id);
 	for (server in bot.servers) {
+		console.log("Servers:" + server.id);
 		for (member in server.members) {
 			console.log("Member:" + member.id);
 			if(oldUser.equals(member)){
