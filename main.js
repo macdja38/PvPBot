52e192487eabbd3dd1f13300d6186ac8417c7980
Jacob <jakeincanada@icloud.com>
2015-11-16 13:29:35 +1100

    escape :

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ bot.on("message", (msg) => {
 		
 		if(msg.content.toLowerCase.indexOf("how do i build a tardis") > -1 && msg.content[0] !== '!'){
 			//display link to tardis site!
-			bot.reply(msg, 'http://eccentricdevotion.github.io/TARDIS/creating-a-tardis.html');				);
+			bot.reply(msg, 'http\://eccentricdevotion.github.io/TARDIS/creating-a-tardis.html');				);
 		}
 		
 		//check if user sent command
