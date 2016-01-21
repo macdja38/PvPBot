b97c94d043a46a58b663e2089a38a34afb8b54fb
Jacob <jakeincanada@icloud.com>
2016-01-21 12:47:20 -0500

    fixed get rolls command

--- main.js
+++ main.js
@@ -241,8 +241,8 @@ bot.on("message", (msg) => {
 				for (i in msg.mentions) {
 					var user = msg.mentions[i];
 					var roles = '';
-					for (j in server.rolesOf(user)) {
-						roles += server.rolesOf(user)[j].id + ',';
+					for (j in msg.server.rolesOf(user)) {
+						roles += msg.server.rolesOf(user)[j].id + ',';
 					}
 					bot.reply(msg, '```' + user + 'has' + roles + '```');
 				}
