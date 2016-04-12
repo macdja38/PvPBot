585b6aa9f910727fe506c1ee833fed4acda38f55
Jacob <jakeincanada@icloud.com>
2016-04-12 09:31:11 -0400

    add armor calculator

--- main.js
+++ main.js
@@ -445,6 +445,31 @@ bot.on("message", (m) => {
             }
         });
     }
+    
+    else if (command === '!!armorstats' || command === '!!armor') {
+        (function() {
+            console.log(args.length);
+            if(args.length < 2 || args.length == 3 || rgs.length > 4) {
+                bot.message(m.channel, "```xl/npossible uses include:\n +
+                    "!!armor (Base Armor) (Base Level) (Current Level) calculate armour and stats.\n" + 
+                    "!!armor (Current Armor)\n```");
+                return;
+            }
+            var text = "```xl\n";
+            if(args.length == 4) {
+                var armor = args[1] × (1 + ((args[2] − args[3])1.75 ÷ 200))
+                text += "at level " + args[2] + " your enemy would have " + armor + " Armor\n"
+            }
+            text += "gives it " + armor / (armor + 300) * 100 + "% damage reduction\n"
+            bot.message(m.channel, text + ```");
+        })();
+    }
+    
+    
+    
+    
+    
+    
 
     /*
      locked commands past this point
